3aa4f9bc5bda3b9ab1b0ce36e1dbc2192fc55253
Success Imamun <success.imamun@decagon.dev>
2022-10-06 10:17:37 +0100

    Final commit. may update code later

--- MimicDataring/Program.cs
+++ MimicDataring/Program.cs
@@ -7,12 +7,16 @@
         static void Main(string[] args)
         {
 
+            //trying to push to decagon repo
+
             //Queue<int> newList = new Queue<int>();
             //newList.Enqueue(43);
             //newList.Enqueue(24);
             //newList.Enqueue(57);
 
             //Console.WriteLine(newList.Dequeue());
+
+            
             
             
         }
